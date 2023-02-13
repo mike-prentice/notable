3dcb2b4c2b923fe02442ac7d3bace81143e50438
Mike Prentice <29170001+mike-prentice@users.noreply.github.com>
2023-02-13 14:58:10 -0500

    project initialization

---
+++ notable/src/main/java/com/notable/notable/NotableApplication.java
@@ -0,0 +1,13 @@
+package com.notable.notable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotableApplication.class, args);
+	}
+
+}
---
+++ notable/src/test/java/com/notable/notable/NotableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notable.notable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
