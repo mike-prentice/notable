145363c40bcf3a221a813f3c4af90dfe2dc8c078
Mike Prentice <29170001+mike-prentice@users.noreply.github.com>
2023-03-07 22:00:42 -0500

    working on shell

--- notable/src/main/java/com/notable/notable/Commands/ParentCommand.java
+++ notable/src/main/java/com/notable/notable/Commands/ParentCommand.java
@@ -1,21 +1,29 @@
 package com.notable.notable.Commands;
 
 import com.notable.notable.Commands.NoteCommands.AddNoteCommand;
+import com.notable.notable.Commands.Shell.ShellCommand;
 
-import java.util.concurrent.Callable;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.ExitCodeGenerator;
 
 import org.springframework.stereotype.Component;
 
 import picocli.CommandLine.HelpCommand;
 import picocli.CommandLine.Option;
 import picocli.CommandLine;
+import picocli.CommandLine.IFactory;
 
 @Component
 @CommandLine.Command(name = "Notable", mixinStandardHelpOptions = true, version = "1.0", description = "Take notes via the command line.", subcommands = {
         AddNoteCommand.class,
+        ShellCommand.class,
         HelpCommand.class
 })
-public class ParentCommand implements Callable<String> {
+@RequiredArgsConstructor
+public class ParentCommand implements Runnable, ExitCodeGenerator {
+    private final IFactory factory;
+    private final ShellCommand shell;
 
     // TODO Add shell
     // TODO Add commands for delete/list/edit to sub command list
@@ -23,15 +31,18 @@ public class ParentCommand implements Callable<String> {
     // TODO Add picocli options in parent command for username and password
     // TODO convert to quarkus/graalVM
 
-    @Option(names = { "-n", "--name" }, description = "Greets the user when their name is entered...")
+    @Option(names = { "-n", "--name" }, description = "A simple command line note taking application")
     private String name;
 
+    private int exitCode;
+    
     @Override
-    public String call() throws Exception {
-        if (name == null) {
-            System.out.println("No Name specified, aborting operation");
-        }
-        System.out.println("Executing option -n: " + this.name + " you are logged in! You are ready to take notes!");
-        return "Hello " + name + " ,you are logged in!";
+    public void run() {
+        exitCode = new CommandLine(shell, factory).execute(new String[] {});
     }
+
+    @Override
+  public int getExitCode() {
+    return exitCode;
+  }
 }
---
+++ notable/src/main/java/com/notable/notable/Commands/Shell/ShellCommand.java
@@ -0,0 +1,119 @@
+package com.notable.notable.Commands.Shell;
+
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+
+
+
+import org.springframework.stereotype.Component;
+
+import com.notable.notable.Commands.NoteCommands.AddNoteCommand;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.HelpCommand;
+import picocli.shell.jline3.PicocliCommands;
+import picocli.shell.jline3.PicocliCommands.PicocliCommandsFactory;
+import picocli.CommandLine;
+
+import org.fusesource.jansi.AnsiConsole;
+import org.jline.console.SystemRegistry;
+import org.jline.console.impl.Builtins;
+import org.jline.console.impl.SystemRegistryImpl;
+import org.jline.keymap.KeyMap;
+import org.jline.reader.*;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.reader.impl.LineReaderImpl;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.widget.TailTipWidgets;
+
+@Command(name = "", description = {
+        "Take notes via the command line." +
+                "Type @|magenta <TAB>|@ to see available commands.",
+        "Type @|magenta ALT-S|@ to toggle tailtips.",
+        "" }, footer = { "", "Press Ctl-D to exit." }, subcommands = {
+                AddNoteCommand.class,
+                HelpCommand.class,
+        })
+@Component
+@Getter
+@RequiredArgsConstructor
+public class ShellCommand implements Runnable {
+    private LineReaderImpl reader;
+    private PrintWriter out;
+
+    private int exitCode;
+
+    final void setReader(LineReader reader) {
+        out = reader.getTerminal().writer();
+    }
+
+    @Override
+    public void run() {
+        ShellCommand shell = new ShellCommand();
+        AnsiConsole.systemInstall();
+        try {
+            Supplier<Path> workDir = () -> Paths.get(System.getProperty("user.home"));
+            Builtins builtins = new Builtins(workDir, null, null);
+            builtins.rename(Builtins.Command.TTOP, "top");
+            builtins.alias("zle", "widget");
+            builtins.alias("bindkey", "keymap");
+
+            PicocliCommandsFactory factory = new PicocliCommandsFactory();
+            CommandLine command = new CommandLine(shell, factory);
+            PicocliCommands picoCommands = new PicocliCommands(command);
+            Parser parser = new DefaultParser();
+            try (Terminal terminal = TerminalBuilder.builder().build()) {
+                SystemRegistry systemRegistry = new SystemRegistryImpl(parser, terminal, workDir, null);
+                systemRegistry.setCommandRegistries(builtins, picoCommands);
+                systemRegistry.register("help", picoCommands);
+
+                LineReader reader = LineReaderBuilder.builder()
+                        .appName("Notable")
+                        .terminal(terminal)
+                        .completer(systemRegistry.completer())
+                        .parser(parser)
+                        .variable(LineReader.LIST_MAX, 50)
+                        .build();
+
+                builtins.setLineReader(reader);
+                shell.setReader(reader);
+                factory.setTerminal(terminal);
+                TailTipWidgets widgets = new TailTipWidgets(reader, systemRegistry::commandDescription, 5,
+                        TailTipWidgets.TipType.COMPLETER);
+                widgets.enable();
+                KeyMap<Binding> keyMap = reader.getKeyMaps().get("main");
+                keyMap.bind(new Reference("tailtip-toggle"), KeyMap.alt("s"));
+
+                String prompt = "Notable> ";
+                String rightPrompt = null;
+
+                String line;
+                while (true) {
+                    try {
+                        systemRegistry.cleanUp();
+                        line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
+                        systemRegistry.execute(line);
+                    } catch (UserInterruptException e) {
+                        // ignore
+                    } catch (EndOfFileException e) {
+                        return;
+                    } catch (Exception e) {
+                        systemRegistry.trace(e);
+                    }
+                }
+            }
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            AnsiConsole.systemUninstall();
+        }
+    }
+
+}
